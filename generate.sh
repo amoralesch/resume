bb4fa2492371409c6fb9be71b158ca3da1ae9bc3
Antonio M <amoralesch@hotmail.com>
2025-11-23 14:27:48 -0500

    Set flag for metadata file

--- generate.sh
+++ generate.sh
@@ -9,18 +9,18 @@ for lang in en es; do
         echo "Generating $style ($lang) ..."
 
         pandoc content/$lang.md \
-            metadata/$lang.yaml \
+            --metadata-file=metadata/$lang.yaml \
             --template=templates/$style.latex \
             --pdf-engine=xelatex \
             -o outputs/${style}_${lang}.pdf
 
         pandoc content/$lang.md \
-            metadata/$lang.yaml \
+            --metadata-file=metadata/$lang.yaml \
             --reference-doc=templates/reference.docx \
             -o outputs/${style}_${lang}.docx
 
         pandoc content/$lang.md \
-            metadata/$lang.yaml \
+            --metadata-file=metadata/$lang.yaml \
             --template=templates/$style.html \
             -o outputs/${style}_${lang}.html
     done
